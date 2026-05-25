584b5823c6cfc49b36c9331d7de7684cc93d56d7
msmidev <128936982+msmidev@users.noreply.github.com>
2026-05-25 23:45:55 +0100

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int current = m + n - 1;
+        int end1 = m - 1;
+        int end2 = n - 1;
+
+        while(end1 >= 0 && end2 >= 0) {
+            if(nums1[end1] <= nums2[end2]) {
+                nums1[current--] = nums2[end2--];
+            } else {
+                nums1[current--] = nums1[end1--];
+            }
+        }
+
+        while(end2 >= 0)
+            nums1[current--] = nums2[end2--];
+    }
+};
