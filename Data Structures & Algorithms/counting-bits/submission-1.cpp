3f30396255d9b69eb031d6797b2022e60dd57549
msmidev <128936982+msmidev@users.noreply.github.com>
2026-05-31 17:01:03 +0100

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> results(n + 1);
+        for(int i = 1; i <= n; i++) {
+            int count = 0;
+            int n = i;
+            while(n) {
+                count++;
+                n = n & (n - 1);
+            }
+            results[i] = count;
+        }
+        return results;
+    }
+};
