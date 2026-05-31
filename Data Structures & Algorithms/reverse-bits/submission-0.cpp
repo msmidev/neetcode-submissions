98c3638ea88b3298a495ec2ea4d7db22bb95fcb0
msmidev <128936982+msmidev@users.noreply.github.com>
2026-05-31 17:38:38 +0100

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int reversed = 0;
+
+        for(int i = 0; i < 32; i++) {
+            int bitSet = (n & (1 << i)) ? 1 : 0;
+            reversed |= bitSet << (32 - i - 1);
+        }
+
+        return reversed;
+    }
+};
