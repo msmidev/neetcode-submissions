5a6b36420ac86be13176c300ba2f37036e04fb50
msmidev <128936982+msmidev@users.noreply.github.com>
2026-05-31 20:21:11 +0100

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int l = 0;
+        int r = matrix.size() * matrix[0].size() - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            int mValue = getValueLinear(matrix, m);
+            if (mValue < target)
+                l = m + 1;
+            else if (mValue > target)
+                r = m - 1;
+            else
+                return true;
+        }
+
+        return false;
+    }
+
+    int getValueLinear(vector<vector<int>>& matrix, int x) {
+        return matrix[x / matrix[0].size()][x % matrix[0].size()];
+    }
+};
